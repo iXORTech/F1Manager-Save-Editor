433e36828f8e75b53555471bb2b6be6a75f94915
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-01-07 20:56:27 -0500

    build: Add version property
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,42 @@
+import java.io.ByteArrayOutputStream
+import java.io.FileInputStream
+import java.util.Properties
+
+val projectProperties = "${projectDir}/project.properties"
+
+fun runShellCommand(command: String, pwd: File = file("./")): String {
+    val byteOut = ByteArrayOutputStream()
+    project.exec {
+        workingDir = pwd
+        commandLine = command.split("\\s".toRegex())
+        standardOutput = byteOut
+    }
+    return String(byteOut.toByteArray()).trim()
+}
+
+fun getProperties(file: String, key: String): String {
+    val fileInputStream = FileInputStream(file)
+    val props = Properties()
+    props.load(fileInputStream)
+    return props.getProperty(key)
+}
+
+val versionProperty: String
+    get() = getProperties(projectProperties, "version")
+
+val stageProperty: String
+    get() = getProperties(projectProperties, "stage")
+
+val revisionProperty: String
+    get() = runShellCommand("git rev-parse --short=7 HEAD")
+
 plugins {
     kotlin("jvm") version "1.8.0"
     application
 }
 
 group = "tech.ixor"
-version = "1.0-SNAPSHOT"
+version = "$versionProperty-$stageProperty+$revisionProperty"
 
 repositories {
     mavenCentral()
